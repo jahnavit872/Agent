fae26ab16657c8d3116f71e85416275b7c8e6339
jahnavit872 <jahnavithippannagari@gmail.com>
2025-12-18 18:54:39 +0530

    updated sanitization

--- Signup.js
+++ Signup.js
@@ -32,8 +32,8 @@ const LoginSchema = Yup.object().shape({
       }
 
       const data = await response.json();
-     
-      alert(`Welcome ${data.username}`);
+      const sanitizedUsername=DOMPurify.sanitize(data.username)
+      alert(`Welcome ${sanitizedUsername}`);
       resetForm();
     } catch (error) {
       alert(error.message);
