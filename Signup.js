2f8825b813e9a7755021e15032ff003bd1543fd2
jahnavit872 <jahnavithippannagari@gmail.com>
2025-12-18 18:40:34 +0530

    imported dompurify

--- Signup.js
+++ Signup.js
@@ -1,5 +1,6 @@
 // src/components/LoginForm.js
 import React from "react";
+import DOMPurify from 'dompurify';
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
