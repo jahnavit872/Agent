67a9bc3a85bb38ef2a87b01f9e16b43de983c539
jahnavit872 <jahnavithippannagari@gmail.com>
2025-12-18 18:04:19 +0530

    updated with sanitization

--- loginForm.js
+++ loginForm.js
@@ -1,5 +1,6 @@
 // src/components/LoginForm.js
 import React from "react";
+import DOMPurify from 'dompurify'
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
@@ -32,7 +33,7 @@ const LoginSchema = Yup.object().shape({
       }
 
       const data = await response.json();
-      const sanitizedUsername = data.username;
+      const sanitizedUsername = DOMPurify.sanitize(data.username);
       alert(`Welcome ${sanitizedUsername}`);
       resetForm();
     } catch (error) {
