0ef2ca066497b23bf1b322217cab7a03a95975eb
jahnavit872 <jahnavithippannagari@gmail.com>
2025-12-23 18:10:36 +0530

    updated

--- hii3.js
+++ hii3.js
@@ -15,9 +15,7 @@ const LoginSchema = Yup.object().shape({
   const handleSubmit = async (values, { setSubmitting, resetForm }) => {
     try {
         const apiUrl = process.env.REACT_APP_API_URL;
-        if (!apiUrl) {
-      throw new Error('API URL is not defined');
-        }
+       
         
  
  
