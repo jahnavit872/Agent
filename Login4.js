56c8e68b2084c6cd6ddaa2eb27fddb91b38a9ece
jahnavit872 <jahnavithippannagari@gmail.com>
2025-12-25 22:25:14 +0530

    updated

--- Login4.js
+++ Login4.js
@@ -4,15 +4,7 @@ import DOMPurify from 'dompurify'
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
-const LoginSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(3, "Username too short")
-    .max(20, "Username too long")
-    .required("Username is required"),
-  password: Yup.string()
-    .min(6, "Password too short")
-    .required("Password is required"),
-});
+
 
 
   const handleSubmit = async (values, { setSubmitting, resetForm }) => {
