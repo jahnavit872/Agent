56952bd860427bbbbe2aa37e53256a0833e058ba
jahnavit872 <jahnavithippannagari@gmail.com>
2025-12-28 20:53:32 +0530

    updated

--- Login11.js
+++ Login11.js
@@ -16,6 +16,9 @@ const LoginSchema = Yup.object().shape({
   const handleSubmit = async (values, { setSubmitting, resetForm }) => {
     try {
         const apiUrl = process.env.REACT_APP_API_URL;
+        if (!apiUrl) {
+      throw new Error('API URL is not defined');
+        }
 
     const response = await fetch(`${apiUrl}/login`, {
         method: "POST",
